2c7e32f9b495392c66bf80a232fb35ef35815ffd
Regomoditsoe <rsmramarumo@gmail.com>
2023-05-29 13:56:09 -0700

    pairwork

--- printf.c
+++ printf.c
@@ -12,9 +12,9 @@ void print_buffer(char buffer[], int *buff_ind);
 int _printf(const char *format, ...)
 {
 	int a, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
+
 	va_list list;
-	char buffer[BUFF_SIZE];
+	
 
 	if (format == NULL)
 		return (-1);
@@ -24,26 +24,10 @@ int _printf(const char *format, ...)
 	for (a = 0; format && format[a] != '\0'; a++)
 	{
 		if (format[a] != '%')
-		{
-			buffer[buff_ind++] = format[a];
-
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-
-			/*write(1, &format[], 1);*/
 			printed_chars++;
-		}
+
 		else
 		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &a);
-			width = get_width(format, &a, list);
-			precision = get_precision(format, &a, list);
-			size = get_size(format, &a);
-			++a;
-			printed = handle_print(dormat, &a, list, buffer,
-					flags, width, precision, size);
-
 			if (printed == -1)
 				return (-1);
 
@@ -51,23 +35,8 @@ int _printf(const char *format, ...)
 		}
 	}
 
-	print_buffer(buffer, &buff_ind);
 
 	va_end(list);
 
 	return (printed_chars);
-}
-
-/**
- * print_buffer - Print the contents of the buffer
- * @buffer: array of characters
- * @buff_ind: index
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
-
 }
