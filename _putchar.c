3540bef6b5aa74466a69b1048420e0466a7147dd
Regomoditsoe <rsmramarumo@gmail.com>
2023-05-30 15:53:50 -0700

    update

--- _putchar.c
+++ _putchar.c
@@ -1,17 +1,4 @@
 #include "main.h"
-/**
- * main - Main function
- *
- * Return: integer
- */
-int main(void)
-{
-	int printed_chars;
-	printed_chars = _printf("Hello, %s! Today is %c. The number is %d\n", "Alice", 'M', 42);
-	printf("Number of characters printed: %d\n", printed_chars);
-	return (0);
-}
-
 
 /**
  * _putchar - prints characters
