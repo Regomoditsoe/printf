7f73a47b87d7eac82327bebb31dc1ac7732850b7
Regomoditsoe <rsmramarumo@gmail.com>
2023-05-30 15:17:34 -0700

    update

--- printf.c
+++ printf.c
@@ -78,3 +78,12 @@ int _printf(const char *format, ...)
 
 	return (printed_chars);
 }
+
+int main (void)
+{
+	int printed_chars;
+
+	printed_chars = _printf("Hello, &s! Today is %c. The number is %d\n", "Alice" "M", 42);
+	printf("Number of characters printed: %d\n", printed_chars);
+	return (0);
+}
