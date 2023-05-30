36f8c2633f9407fe99415e2ab58b5db8f9f95a11
Regomoditsoe <rsmramarumo@gmail.com>
2023-05-30 06:40:13 -0700

    Pair work

--- printf.c
+++ printf.c
@@ -3,36 +3,47 @@
 #include <stdio.h>
 
 /**
- * p_percent - declaring the function for percent as unused
+ * p_percent - declaring the function for percent specifier
  * @args: the argument passed
- * Return: 0
+ * Return: the number of characters printed
 */
 int p_percent(va_list args)
 {
 	(void)args; /** Mark the parameter as unused */
-	return (0);
+	putchar('%');
+	return (1);
 }
 
 /**
- * p_char - declaring the function for c specifier as unused
+ * p_char - declaring the function for c specifier
  * @args: the argument passed
- * Return: 0
+ * Return: the number of characters printed
 */
 int p_char(va_list args)
 {
-	(void)args; /** Mark the parameter as unused */
-	return (0);
+	char c = (char)va_arg(args, int);
+
+	putchar(c);
+	return (1);
 }
 
 /**
- * p_string - declaring the function for s specifier as unused
+ * p_string - declaring the function for s specifier
  * @args: the argument passed
- * Return: 0
+ * Return: the number of characters printed
 */
 int p_string(va_list args)
 {
-	(void)args; /** Mark the parameter as unused */
-	return (0);
+	const char *str = va_arg(args, const char *);
+	int count = 0;
+
+	while (*str != '\0')
+	{
+		putchar(*str);
+		str++;
+		count++;
+	}
+		return (count);
 }
 
 /**
@@ -42,6 +53,7 @@ int p_string(va_list args)
  * @args: the input argument
  * Return: 0
 */
+
 int p_all(const char *format, convert_t *func_list, va_list args)
 {
 	(void)args; /** Mark the parameter as unused */
@@ -63,8 +75,6 @@ int _printf(const char *format, ...)
 		{"%", p_percent},
 		{"c", p_char},
 		{"s", p_string},
-		{"d", p_integer},
-		{"i", p_integer},
 		{NULL, NULL}
 	};
 
@@ -74,8 +84,6 @@ int _printf(const char *format, ...)
 		return (-1);
 
 	va_start(args, format);
-
-		/** Call a parser function */
 
 		printed_chars = p_all(format, func_list, args);
 
